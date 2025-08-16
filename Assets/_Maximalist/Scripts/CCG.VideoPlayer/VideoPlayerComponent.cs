c18f0649b0e1573c73ecf647dd2526cdd5983558
Corrin Wilson <corrin.wilson95@gmail.com>
2025-08-16 11:42:12 +0100

    Added url version of video player

--- Assets/_Maximalist/Scripts/CCG.VideoPlayer/VideoPlayerComponent.cs
+++ Assets/_Maximalist/Scripts/CCG.VideoPlayer/VideoPlayerComponent.cs
@@ -1,14 +1,61 @@
 using UnityEngine;
+using UnityEngine.Video;
 
-public class VideoPlayerComponent: MonoBehaviour
+namespace CCG.Video 
 {
-    [SerializeField] private RenderTexture renderTexture;
-
-    [ContextMenu("SetRenderTextureAnisoLevelTo0")]
-    void Start()
+    public class VideoPlayerComponent : MonoBehaviour
     {
-        renderTexture.anisoLevel = 0;
+        [Header("Video Setup")]
+        [SerializeField] private VideoPlayer videoPlayer;
+        [SerializeField] private RenderTexture renderTexture;
+
+        [Header("Loading UI")]
+        [SerializeField] private GameObject loadingSpinner; // Assign your sprite/anim in inspector
+        [SerializeField] private float spinSpeed = 180f; // Degrees per second
+
+        private bool isPreparing = true;
+
+        private void Start()
+        {
+            renderTexture.anisoLevel = 0;
+
+            // Show loading spinner
+            if (loadingSpinner != null)
+                loadingSpinner.SetActive(true);
+
+            isPreparing = true;
+
+            // Hook events
+            videoPlayer.prepareCompleted += OnVideoPrepared;
+            videoPlayer.loopPointReached += OnVideoEnded; // optional
+
+            // Start preparing
+            videoPlayer.Prepare();
+        }
+
+        private void Update()
+        {
+            // Rotate loading sprite if preparing
+            if (isPreparing && loadingSpinner != null)
+            {
+                loadingSpinner.transform.Rotate(Vector3.forward, -spinSpeed * Time.deltaTime);
+            }
+        }
+
+        private void OnVideoPrepared(VideoPlayer vp)
+        {
+            isPreparing = false;
+
+            // Hide spinner
+            if (loadingSpinner != null)
+                loadingSpinner.SetActive(false);
+
+            vp.Play();
+        }
+
+        private void OnVideoEnded(VideoPlayer vp)
+        {
+            // Optional: show spinner again, or handle UI
+        }
     }
-
-
 }
