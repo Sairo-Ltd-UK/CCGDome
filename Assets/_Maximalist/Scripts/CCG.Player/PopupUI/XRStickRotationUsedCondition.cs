2ad32a943af4220e3638b131199eda26225b438a
Corrin Wilson <corrin@maximalistltd.com>
2025-08-25 13:37:53 +0100

    Started work on prompt system

---
+++ Assets/_Maximalist/Scripts/CCG.Player/PopupUI/XRStickRotationUsedCondition.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Locomotion;
+using UnityEngine.XR.Interaction.Toolkit.Locomotion.Turning;
+
+namespace CCG.Player.Prompt
+{
+	public class XRStickRotationUsedCondition : CompletionCondition
+	{
+		[SerializeField] private SnapTurnProvider provider;
+		private bool rotated = false;
+
+		public override void OnBegin()
+		{
+			rotated = false;
+
+			if (provider != null)
+			{
+				provider.locomotionEnded += OnTurn;
+			}
+		}
+
+		private void OnTurn(LocomotionProvider eventProvider)
+		{
+			Debug.Log("[RUC] OnTurn");
+
+			if (eventProvider != provider)
+				return;
+
+			rotated = true;
+
+			if (provider != null)
+				provider.locomotionEnded -= OnTurn;
+		}
+
+		[ContextMenu("TestCheckComplete")]
+		public void TestCheckComplete()
+		{
+			rotated = true;
+		}
+
+		public override bool CheckComplete()
+		{
+			return rotated;
+		}
+	}
+}
