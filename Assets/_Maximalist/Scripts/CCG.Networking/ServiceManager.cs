2f122bf667bcdcef6260bbfb266da89f194c277d
Corrin Wilson <corrin@maximalistltd.com>
2025-08-26 11:35:43 +0100

    Editor now starts a client to connect to servers.

--- Assets/_Maximalist/Scripts/CCG.Networking/ServiceManager.cs
+++ Assets/_Maximalist/Scripts/CCG.Networking/ServiceManager.cs
@@ -34,8 +34,10 @@ namespace CCG.Networking
 
 #if UNITY_EDITOR
 
-				NetworkManager.singleton.StartHost();
+				//NetworkManager.singleton.StartHost();
 
+				await AuthenticationService.InitializeAsync();
+				await MatchmakingClient.RequestMatchAsync();
 
 #elif UNITY_SERVER
 
